36e1a779428a801fb7e87406e60d049e98c181bf
DESKTOP-DSRHOUV\joran <joranvanerk1@gmail.com>
2021-04-20 09:30:50 +0200

    remove
    removed old bugs

--- paneel/connect_db.php
+++ paneel/connect_db.php
@@ -3,9 +3,9 @@
 
     //gegevens voor database inloggen
     $servername = "localhost";
-    $username = "samensoc_wiki";
-    $password = "H15ndlZWLL";
-    $dbname = "samensoc_wiki";
+    $username = "***";
+    $password = "***";
+    $dbname = "***";
 
     //inloggen bij de database
     $conn = mysqli_connect($servername, $username, $password, $dbname);
